2c2439a5c848a3d30b2cdd6ecf90cbef49646598
Complexia <complexia701@gmail.com>
2023-05-23 07:41:23 +1000

    set up the UI and some basic rules. conception coming together

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -13,18 +13,45 @@ export const App = () => {
 
   return (
     <>
-      <div className="container mx-4">
-        Counter: <span className="text-3xl font-bold underline">{counter?.value ?? "??"}</span>
+      <div className="flex flex-row">
+        <div className="flex flex-col">
+          <div className="bg-gray-800 rounded-lg p-4 text-white flex mx-5 px-5 my-1"> 
+            <div className="flex flex-col">
+              <span className="mx-3">World</span>
+              <span className="mx-3">Rules: </span>
+              
+              <div className="button bg-gray-200 rounded-lg px-4  mt-5 ml-10 text-gray-800">
+                <button>
+                  Add Rule
+                </button>
+              </div>
+              
+            </div>
+            
+          </div>
+          
+          <div className="bg-gray-800 rounded-lg p-4 text-white flex items-center justify-center mx-5 px-5 my-1"> 
+            <div className="flex flex-col">
+              <span>Autonomous agents</span>
+              <span>Agents: </span>
+
+              <div className="button bg-gray-200 rounded-lg px-4  mt-5 ml-10 text-gray-800">
+                <button>
+                  Add Agent
+                </button>
+              </div>
+
+            </div>
+
+          </div>
+        </div>
+        
+        <div className="bg-gray-800 rounded-lg p-4 text-white flex items-center justify-center mx-5 px-5 my-1 w-auto"> 
+          Play
+        </div>
+        
       </div>
-      <button
-        type="button"
-        onClick={async (event) => {
-          event.preventDefault();
-          console.log("new counter value:", await increment());
-        }}
-      >
-        Increment
-      </button>
+      
       
     </>
   );
